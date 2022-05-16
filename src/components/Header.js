c19552c3d6babe8bf37cedd0ec20d14deed5f928
unknown <ghekghek2@gmail.com.com>
2022-05-17 07:54:32 +0800

    add files

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+
+    <div className=" container-lg header justify-content-between p-3 ">
+    <div className="logo">
+      <div className="img-logo">
+      </div>
+      <h4>
+        Meme Generator
+      </h4>
+    </div>
+  
+  <div className="title">
+    <p>React Course - Project 3</p>
+  </div>
+  
+  </div>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Main() {
+  return (
+<div className=" container-lg main mt-5 row justify-content-center">
+
+   
+
+
+    
+
+<input className='  col-md-5  ' type="text" />
+
+
+<input className='  col-md-5  ' type="text" />
+
+
+
+
+<button className='mt-3 col-10  btn btn-primary'>Get a new meme image</button>
+
+
+<img src="" alt="" />
+
+</div>
+
+  )
+}
+
+export default Main
